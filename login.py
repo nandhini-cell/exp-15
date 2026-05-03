daecda89715fc650b0ce86cbe860d2a63ee9f2c8
nandhini-cell <192472297.simats@saveetha.com>
2026-05-03 21:13:46 +0530

    added login function

---
+++ login.py
@@ -0,0 +1,9 @@
+def login(username, password):
+    if username == "admin" and password == "1234":
+        return "Login successful"
+    else:
+        return "Invalid credentials"
+
+
+# Test the function
+print(login("admin", "1234"))
